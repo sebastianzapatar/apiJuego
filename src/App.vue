3e22d0fb7a8619da653b63b9520bf5ed9edaedd1
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-04-24 10:38:56 -0500

    Se crea el ApiVueConection

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>I forgot Chi-cuadrado fuck ( ´･･)ﾉ(._.`)</h1>
+  <ApiConectionVue/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApiConectionVue from './components/ApiConection.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApiConectionVue     
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ApiConection.vue
@@ -0,0 +1,72 @@
+<template>
+    <h2>Aske about anything you want, only yes/no Questions</h2>
+    <img :src="image" alt="bg"/>
+    <div class="bg-dark">
+        <div class="indesicion-container">
+            <input type="text" placeholder="ask mother fucker" v-model="question"/>
+            <p>Please use ? at the end</p>
+            <div v-if="isValidQuestion">
+                <h2>{{ question }}</h2>
+                <h1>{{ answer }}</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            question:'',
+            answer:null,
+            image:'https://via.placeholder.com/250',
+            isValidQuestion:false
+        }
+    }
+}
+</script>
+
+<style>
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+</style>
